b51fc4b9934e50b36c8ab5dea6e18ddaaae39d7f
Pablo2733s <Pablo2733v@gmail.com>
2023-12-05 14:16:13 -0300

    oi

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(cors({
-  origin: 'https://site-inep-escolas.netlify.app/',
+  origin: '*',
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   credentials: true,
 }));
